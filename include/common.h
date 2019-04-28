668a50bcb5f2b29449621e61a46ca1dbf804e9ff
Zhi Sun <inhzus@gmail.com>
2019-04-28 11:51:58 +0800

    [Fix] commit fix: common header

---
+++ include/common.h
@@ -0,0 +1,13 @@
+//
+// Copyright [2019] <Zhi Sun>
+//
+
+#ifndef REVIEW_COMMON_H
+#define REVIEW_COMMON_H
+
+#include <vector>
+#include <string>
+using std::string;
+using std::vector;
+
+#endif //REVIEW_COMMON_H
